9f55b38d9eabd0d2d5b5ac414712193db956500d
RanukaDinsitha <ranuka@outlook.co.nz>
2023-07-27 17:33:13 +1200

    Small Change Commit

---
+++ src/components/console-manager.cjs
@@ -0,0 +1,6 @@
+console.log("Welcome To The Dev Console!");
+console.log("Please don't paste anything in here.");
+console.log("It can can change how you view the site!!");
+console.log("Thank You For Visiting Also!!!!!!!!!!");
+console.log(":-) :D :)");
+// By Ranuka //
