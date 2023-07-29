417bcc5beec71a172f2e4ce0a26ba1a9c161123b
RanukaDinsitha <ranuka@outlook.co.nz>
2023-07-30 05:33:45 +1200

    Small Update?

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,10 @@ import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
 // import tailwind from "@astrojs/tailwind";
 import partytown from "@astrojs/partytown";
+import typescript from "@astrojs/ts-plugin";
+import ckeditor5 from "@ckeditor/ckeditor5-build-classic";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), partytown()],
+  integrations: [react(), partytown(), typescript(), ckeditor5()],
 });
