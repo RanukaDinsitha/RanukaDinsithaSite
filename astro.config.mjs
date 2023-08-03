89711e6b95807a66bb5d6432240968867cdbc797
RanukaDinsitha <ranuka@outlook.co.nz>
2023-08-04 06:38:14 +1200

    Holy Moly Uninstalled A BUNCH Of Stuff :( Due To A Error

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,18 +2,17 @@ import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
 // import tailwind from "@astrojs/tailwind";
 import partytown from "@astrojs/partytown";
-import typescript from "@astrojs/ts-plugin";
+// import typescript from "@astrojs/ts-plugin";
 // import ckeditor5 from "@ckeditor/ckeditor5-build-classic";
 import vue from "@astrojs/vue";
-import node from "@astrojs/node";
+// import node from "@astrojs/node";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), partytown(), typescript(), vue(), node()],
+  integrations: [react(), partytown(), vue()],
   output: "server",
-  adapter: node({
-    mode: "standalone",
-  }),
+  // adapter: node({
+  // mode: "standalone",
 });
 
 // Astro Config File for Site
