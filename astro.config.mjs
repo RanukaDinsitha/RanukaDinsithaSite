00142a2177f3ad4e475389742deb1e7313ac8115
RanukaDinsitha <ranuka@outlook.co.nz>
2023-08-06 04:50:48 +1200

    Git Update! (INITAL COMMIT)

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,8 @@ import vue from "@astrojs/vue";
 import partytown from "@astrojs/partytown";
 import image from "@astrojs/image";
 import node from "@astrojs/node";
+import vercel from "@astrojs/vercel/serverless";
+import svelte from "@astrojs/svelte";
 // import cloudflare from "@astrojs/cloudflare"
 // import node from "@astrojs/node";
 // import tailwind from "@astrojs/tailwind";
@@ -12,9 +14,6 @@ import node from "@astrojs/node";
 // import bootstrap from "bootstrap";
 // import { ts, typescript } from "@astrojs/ts-plugin";
 
-import vercel from "@astrojs/vercel/serverless";
-
-import svelte from "@astrojs/svelte";
 
 // https://astro.build/config
 export default defineConfig({
