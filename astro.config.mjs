aae5c420f0b0f17557f0a23d50ddec2b207d8ac9
RanukaDinsitha <ranuka@outlook.co.nz>
2023-08-15 15:17:10 +1200

    Astro Update!

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,10 +3,15 @@ import react from '@astrojs/react'
 import vue from '@astrojs/vue'
 import partytown from '@astrojs/partytown'
 import image from '@astrojs/image'
-import node from '@astrojs/node'
-import vercel from '@astrojs/vercel/serverless'
 import svelte from '@astrojs/svelte'
+import mdx from '@astrojs/mdx'
+import starlight from '@astrojs/starlight'
+import robotsTxt from 'astro-robots-txt'
+import solidJs from '@astrojs/solid-js'
+import preact from '@astrojs/preact'
 
+// import node from '@astrojs/node'
+// import vercel from '@astrojs/vercel/serverless'
 // import from MRT "@astrojs/MRT"
 // import cloudflare from "@astrojs/cloudflare"
 // import node from "@astrojs/node";
@@ -16,11 +21,33 @@ import svelte from '@astrojs/svelte'
 // import bootstrap from "bootstrap";
 // import { ts, typescript } from "@astrojs/ts-plugin";
 
-// Helped By Rangana! :D rangana is his github username! Please help him!
+// Helped By Rangana! :D rangana is his github username!
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [react(), partytown(), vue(), image(), svelte()],
-	output: 'server',
-	adapter: vercel()
+	integrations: [
+		react(),
+		partytown(),
+		vue(),
+		image(),
+		svelte(),
+		starlight(),
+		mdx({
+			syntaxHighlight: 'shiki',
+			shikiConfig: {
+				theme: 'dracula'
+			},
+			remarkPlugins: [remarkToc],
+			rehypePlugins: [rehypeMinifyHtml],
+			remarkRehype: {
+				footnoteLabel: 'Footnotes'
+			},
+			gfm: false,
+			extendMarkdownConfig: false,
+			optimize: true
+		}),
+		solidJs(),
+		robotsTxt({}),
+		preact()
+	]
 })
