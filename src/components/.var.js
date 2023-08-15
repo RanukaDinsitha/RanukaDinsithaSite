8557a029bda0ad76203aedb43f19cdef1df20a8b
RanukaDinsitha <ranuka@outlook.co.nz>
2023-08-15 15:57:24 +1200

    var

---
+++ src/components/.var.js
@@ -0,0 +1,13 @@
+// Var defs.
+
+var webFramework = astro
+var nodeSite = true
+var astroSite = true
+var welcomeMes = Welcome
+var devNoteShow = remebererToFixTheBackupRestartDEVNOTEONLYFORDEVSIFYOUARENOTaDEVDONOTRUNTHISCOMMAND
+var whoAreYou = whatDoYouThinkIAmAGeene
+
+welcomeMes
+
+true && false
+true
