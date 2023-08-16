50936f3fd6d7aeb569d66dc1a180c23013b88c06
RanukaDinsitha <ranuka@outlook.co.nz>
2023-08-16 16:23:10 +1200

    PostCSS Config!

--- src/components/.var.js
+++ src/components/.var.js
@@ -1,13 +1,12 @@
-// Var defs.
+// VAR DEFS.
 
 var webFramework = astro
 var nodeSite = true
 var astroSite = true
-var welcomeMes = Welcome
+var welcomeMessage = Welcome
 var devNoteShow = remebererToFixTheBackupRestartDEVNOTEONLYFORDEVSIFYOUARENOTaDEVDONOTRUNTHISCOMMAND
 var whoAreYou = whatDoYouThinkIAmAGeene
-
-welcomeMes
+welcomeMessage
 
 true && false
 true
